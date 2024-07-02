c9e145ca709d7d236dfcb158e9115749ebb9b38a
xiaoli-web <75812684+xiaoli-web@users.noreply.github.com>
2024-07-02 13:48:33 +0200

    Update code.r

--- code.r
+++ code.r
@@ -1,6 +1,6 @@
 # Code for the analysis and plot for the study: Sex-specific epigenetic signatures of circulating urate and its increase after BCG vaccination
 
-# Quality control of the DNA methylation data was described in paper: https://github.com/CiiM-Bioinformatics-group/BCG_methylation_project
+# Quality control of the DNA methylation data was described in the paper: https://github.com/CiiM-Bioinformatics-group/BCG_methylation_project
 
 # Below is the code for performing epigenome-wide association analysis and plotting
 # Load required libraries
@@ -178,6 +178,45 @@ setcolorder(result, c("probeID", "BETA", "SE", "P_VAL"))
 result$padj <- p.adjust(result$P_VAL, method = "BH")
 
 write.xlsx(result, file = "output/rlm_v1_female_mval_trimmed_methy_dependent.xlsx")
+
+# EWAS of urate change
+load("input/m1.trim.rdata")
+load("input/uv1.rdata")
+load("input/uv2.rdata")
+load("input/pdv1.rdata")
+
+mv1.t <- t(m1.trim)
+
+uv2_2 <- uv2[which(uv2$name %in% uv1$name),]
+sum(uv2_2$name == uv1$name)
+uv2_minus_uv1 <- data.frame(uv2_2 = uv2_2$con, uv1=uv1$con)
+uv2_minus_uv1$minus <- uv2_minus_uv1$uv2_2-uv2_minus_uv1$uv1
+
+uv2_minus_uv1$minus <- qnorm((rank(uv2_minus_uv1$minus, na.last="keep") - 0.5) / sum(!is.na(uv2_minus_uv1$minus)))
+
+pdv1$Sample_Plate <- droplevels(pdv1$Sample_Plate)
+str(pdv1)
+
+RLMtest = function(meth_matrix, methcol, urate, age, gender, plate, CD8, CD4, NK, B, Mono, Neu) {mod = try(rlm(urate ~ meth_matrix[, methcol]+age+gender+plate+CD8+CD4+NK+B+Mono+Neu, maxit=200))
+if(class(mod) == "try-error"){
+  print(paste("error thrown by column", methcol))
+  invisible(rep(NA, 3))
+}else cf = coeftest(mod, vcov=vcovHC(mod, type="HC0"))
+cf[2, c("Estimate", "Std. Error", "Pr(>|z|)")]
+}
+
+res <- mclapply(setNames(seq_len(ncol(mv1.t)), dimnames(mv1.t)[[2]]), RLMtest, meth_matrix=mv1.t, urate=uv2_minus_uv1$minus, age=pdv1$age, gender=pdv1$gender, plate=pdv1$Sample_Plate, CD8=pdv1$CD8T, CD4=pdv1$CD4T, NK=pdv1$NK, B=pdv1$Bcell, Mono=pdv1$Mono, Neu=pdv1$Neu)
+setattr(res, 'class', 'data.frame')
+setattr(res, "row.names", c(NA_integer_,4))
+setattr(res, "names", make.names(names(res), unique=TRUE))
+probelistnamesB <- names(res)
+result <- t(data.table(res))
+result<-data.table(result)
+result[, probeID := probelistnamesB]
+setnames(result, c("BETA","SE", "P_VAL", "probeID")) # rename columns
+setcolorder(result, c("probeID","BETA","SE", "P_VAL"))
+result$padj <- p.adjust(result$P_VAL, method ="BH")
+write.xlsx(result, file = "output/all_v2_minus_v1_mval_trimmed_urate_rank.xlsx")
 
 # Manhattan plot
 gwas_data_load <- read_excel("output/EWAS_result.xlsx")
